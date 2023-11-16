fdbadc9e4e9a00ce092baf7fa0af78d420275827
clement <wildcat6@gmail.com>
2023-11-16 14:37:28 +0100

    ancien non envoyé ?

---
+++ assets/js/engine.js
@@ -0,0 +1,16 @@
+
+// const javaUl = document.querySelector('#java');
+// const innerUl = javaUl.querySelector('ul');
+// const innerLi = javaUl.querySelector('li');
+
+// javaUl.addEventListener('click', () => {
+//   innerUl.classList.toggle('hidden');
+//   if (!innerUl.classList.contains('hidden')) {
+//     javaUl.classList.add('show');
+//     innerUl.style.maxHeight = innerUl.scrollHeight + "px";
+//     innerLi.style.maxHeight = innerLi.scrollHeight + "px";
+//   } else {
+//     javaUl.classList.remove('show');
+//     innerUl.style.maxHeight = null;
+//   }
+// });
